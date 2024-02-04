8b4938deb8aeb7cb0c40361ca006975fd4c694d0
har1nagle <hnagle.0@yahoo.com>
2024-02-04 22:04:06 +0530

    Started adding basic redux setup with toolkit

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -7,11 +7,17 @@ import './index.css';
 // Library imports
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './Redux/store.js';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 )
