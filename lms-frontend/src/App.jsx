f74cb2e6db9761e1a8c57fce6b8559cfab931c2e
har1nagle <hnagle.0@yahoo.com>
2024-02-13 16:07:06 +0530

    Added 404 not found page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="*" element= {<NotFound/>}></Route>
       </Routes>
     </>
   );
